af4f4e8dc46f94990880e8a1ab5f5cd3502ae4a3
Yanuarprayoga9 <yanuarprayogat@gmail.com>
2024-01-30 23:38:14 +0700

    feat: modal store

--- components/ui/modal.tsx
+++ components/ui/modal.tsx
@@ -26,6 +26,7 @@ export const Modal: React.FC<ModalProps> = ({
 }) => {
   const onChange = (open: boolean) => {
     if (!open) {
+      //onchange jka open false maka onclose
       onClose();
     }
   };
