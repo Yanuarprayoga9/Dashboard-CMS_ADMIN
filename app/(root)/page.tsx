b2ea1d2ebe6ca170b21250f7c4131ce3fb70f9bf
Yanuarprayoga9 <yanuarprayogat@gmail.com>
2024-01-31 00:35:37 +0700

    fix: fixing bug in modal store
    fixed using useEffect

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,13 +1,16 @@
 "use client";
-import { Modal } from "@/components/ui/modal";
-import { SignInButton, SignedOut, UserButton, auth } from "@clerk/nextjs";
-import { DialogTrigger } from "@radix-ui/react-dialog";
+import { useStoreModal } from "@/hooks/use-store-modal";
+
+import { useEffect } from "react";
 
 export default function Home() {
-  return (
-    <>
-      <UserButton afterSignOutUrl="/sign-in" />
-    
-    </>
-  );
+  const onOpen = useStoreModal((state) => state.onOpen);
+  const isOpen = useStoreModal((state) => state.isOpen);
+
+  useEffect(() => {
+    if (!isOpen) onOpen();
+  }, [isOpen, onOpen]);
+  return <>
+  
+  </>;
 }
