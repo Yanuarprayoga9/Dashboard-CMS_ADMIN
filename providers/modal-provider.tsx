ba5064e296cc2b317db7547bf2b4ed00b9b728bd
Yanuarprayoga9 <yanuarprayogat@gmail.com>
2024-01-30 23:51:49 +0700

    perf: ensure modal not render in SSR
    return null untill component Is Mounted

--- providers/modal-provider.tsx
+++ providers/modal-provider.tsx
@@ -1,14 +1,20 @@
 "use client";
 
+import { StoreModal } from "@/components/modals/store-modal";
 import { useEffect, useState } from "react";
 
 export const ModalProvider = () => {
   const [isMounted, setIsMounted] = useState<boolean>(false);
 
   useEffect(() => {
-    setIsMounted(true)
-
+    setIsMounted(true);
   }, []);
 
   if (!isMounted) return null;
+
+  return (
+    <>
+      <StoreModal />
+    </>
+  );
 };
