23e58a66488857400f734c649d5c270b4d27c7fc
Yanuarprayoga9 <yanuarprayogat@gmail.com>
2024-02-01 19:38:51 +0700

    fix: params storeid
    change name params storeid to storeId

--- app/(dashboard)/[storeid]/layout.tsx
+++ app/(dashboard)/[storeid]/layout.tsx
@@ -1,26 +1,42 @@
-import { prismadb } from "@/lib/db";
-import { auth } from "@clerk/nextjs";
 import { redirect } from "next/navigation";
+import { auth } from "@clerk/nextjs";
 
-export const DashboardLayout = async (
-  children: React.ReactNode,
-  params: { storeId: string }
-) => {
+import { prismadb } from "@/lib/db";
+import Navbar from "@/components/navbar";
+
+export default async function DashboardLayout({
+  children,
+  params,
+}: {
+  children: React.ReactNode;
+  params: { storeId: string };
+}) {
   const { userId } = auth();
-  if (!userId) redirect("/sign-in");
+
+  if (!userId) {
+    redirect("/sign-in");
+  }
 
   const store = await prismadb.store.findFirst({
     where: {
       id: params.storeId,
+      userId,
     },
   });
 
-  if (!store) redirect("/");
+  if (!store) {
+    redirect("/");
+  }
+
+  /*
+   * This layout checked user login
+   * check store wtih params storeId
+   */
 
   return (
     <>
-      <div className="">This is wiill be navbar</div>
+      <Navbar />
       {children}
     </>
   );
-};
+}
