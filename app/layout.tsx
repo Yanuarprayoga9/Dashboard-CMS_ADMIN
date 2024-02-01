9edcfd0a338a802742219881c4b323dfd0a462b6
Yanuarprayoga9 <yanuarprayogat@gmail.com>
2024-02-01 19:37:52 +0700

    style: change ;

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import { ClerkProvider } from "@clerk/nextjs";
 import { ModalProvider } from "@/providers/modal-provider";
 import { ToasterProvider } from "@/providers/toast-provider";
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ["latin"], variable: "--font-sans" });
 
 export const metadata: Metadata = {
   title: "Admin Dashboard",
@@ -21,7 +21,7 @@ export default function RootLayout({
     <ClerkProvider>
       <html lang="en">
         <body className={inter.className}>
-          <ToasterProvider/>
+          <ToasterProvider />
           <ModalProvider />
           {children}
         </body>
