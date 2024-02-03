eb717e0bdd5054518b723e7506b377532c6ae986
Yanuarprayoga9 <yanuarprayogat@gmail.com>
2024-02-03 08:59:47 +0700

    fix: bug cant delete store

--- app/api/stores/[storeId]/route.ts
+++ app/api/stores/[storeId]/route.ts
@@ -39,25 +39,30 @@ export async function PATCH(
 }
 
 export async function DELETE(
+  req: Request,
   { params }: { params: { storeId: string } }
-
 ) {
   try {
     const { userId } = auth();
-    if (!userId) return new NextResponse("Unauthenticated", { status: 403 });
 
-    if (!params.storeId)
-      return new NextResponse("store id is required", { status: 400 });
+    if (!userId) {
+      return new NextResponse("Unauthenticated", { status: 403 });
+    }
+
+    if (!params.storeId) {
+      return new NextResponse("Store id is required", { status: 400 });
+    }
 
     const store = await prismadb.store.deleteMany({
       where: {
         id: params.storeId,
-        userId,
-      },
+        userId
+      }
     });
+  
     return NextResponse.json(store);
   } catch (error) {
-    console.log("[STORE_DELETE]", error);
-    return new NextResponse("Something went wrong", { status: 500 });
+    console.log('[STORE_DELETE]', error);
+    return new NextResponse("Internal error", { status: 500 });
   }
-}
+};
