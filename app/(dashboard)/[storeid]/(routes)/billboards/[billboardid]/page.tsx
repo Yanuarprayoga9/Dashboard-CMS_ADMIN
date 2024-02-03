d26936e4eb48f2e992d8963767986bb78010b7a8
Yanuarprayoga9 <yanuarprayogat@gmail.com>
2024-02-03 14:40:55 +0700

    feat: billboard id page

---
+++ app/(dashboard)/[storeid]/(routes)/billboards/[billboardid]/page.tsx
@@ -0,0 +1,14 @@
+"use server"
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className='flex flex-col p-4'>
+        <div className="flex-1 space-y-4 p-8 pt-6">
+            this page will be billboard form
+        </div>
+    </div>
+  )
+}
+
+export default page
