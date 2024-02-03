cf1ecfafaf51158b8c160961d88dfee939727680
Yanuarprayoga9 <yanuarprayogat@gmail.com>
2024-02-03 10:52:27 +0700

    style: global origin api

---
+++ hooks/use-origin.tsx
@@ -0,0 +1,15 @@
+"use client"
+import { useState,useEffect, use } from "react";
+
+
+export const useOrigin = () => {
+    const [mounted,setMounted] = useState(false)
+    const origin = typeof window !== "undefined" && window.location.origin ?  window.location.origin : '';
+
+    useEffect(()=>{
+        setMounted(true)
+    },[])
+
+    if(!mounted) return null
+    return origin
+}
