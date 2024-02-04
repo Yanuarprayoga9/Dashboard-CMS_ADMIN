c4b958a9d54faca938598054239ae39cbc5892b9
Yanuarprayoga9 <yanuarprayogat@gmail.com>
2024-02-04 17:20:34 +0700

    feat: cloudinary file upload

--- components/ui/heading.tsx
+++ components/ui/heading.tsx
@@ -8,7 +8,7 @@ export const Heading:React.FC<HeadingProps> = ({
     title,descriprtion
 }) => {
   return (
-    <div className='ml-4'>
+    <div className=''>
         <h2  className='text-3xl font-bold tracking-tight'>{title}</h2>
         <p className='text-sm text-muted-foreground'>{descriprtion}</p>
     </div>
--- components/ui/image-upload.tsx
+++ components/ui/image-upload.tsx
@@ -1,14 +1,15 @@
 "use client";
 
-import { Key, Trash } from "lucide-react";
+import { ImagePlus, Key, Trash } from "lucide-react";
 import { useEffect, useState } from "react";
 import { Button } from "./button";
-
+import Image from "next/image";
+import {CldUploadWidget} from "next-cloudinary"
 interface ImageloadProps {
   disabled?: boolean;
   onChange: (value: string) => void;
   onRemove: (value: string) => void;
-  value: string;
+  value: string[];
 }
 export const ImageUpload = ({
   disabled,
@@ -27,26 +28,46 @@ export const ImageUpload = ({
   };
   if (!mounted) return null;
   return (
-    <div className="mb-4 lex flex items-center gap-4">
-      {value.map((url) => (
-        <div
-          key={url}
-          className="relative w-[200px] h-[200px] rounded-md overflow-hidden"
-        >
-          <div className="z-10 absolute top-2 right-2">
-            <Button
-              type="button"
-              onClick={() => {
-                onRemove(url);
-              }}
-              variant="destructive"
-              size="icon"
-            >
-              <Trash className="w-4 h-4" />
-            </Button>
+    <div className="">
+      <div className="mb-4 lex flex items-center gap-4">
+        {value.map((url) => (
+          <div
+            key={url}
+            className="relative w-[200px] h-[200px] rounded-md overflow-hidden"
+          >
+            <div className="z-10 absolute top-2 right-2">
+              <Button
+                type="button"
+                onClick={() => {
+                  onRemove(url);
+                }}
+                variant="destructive"
+                size="icon"
+              >
+                <Trash className="w-4 h-4" />
+              </Button>
+            </div>
+            <Image fill className="object-cover" alt="image" src={url} />
           </div>
-        </div>
-      ))}
+        ))}
+      </div>
+      <CldUploadWidget onUpload={onUpload} uploadPreset="n3o8qwds">
+        {({open})=>{
+          const onClick = () => {
+            open()
+          }
+          return (
+            <Button
+            type="button"
+            disabled={disabled}
+            variant="secondary"
+            onClick={onClick}
+            >
+              <ImagePlus className="h-4 w-4 mr-2"/>
+            </Button>
+          )
+        }}
+      </CldUploadWidget>
     </div>
   );
 };
