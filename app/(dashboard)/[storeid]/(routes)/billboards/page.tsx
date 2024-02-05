229c12524dd5255ce0a14df33789d83f2352a9c6
Yanuarprayoga9 <yanuarprayogat@gmail.com>
2024-02-05 18:40:04 +0700

    feat: data table usage for billboard

--- app/(dashboard)/[storeid]/(routes)/billboards/page.tsx
+++ app/(dashboard)/[storeid]/(routes)/billboards/page.tsx
@@ -1,11 +1,31 @@
+"use server";
 import React from "react";
 import { BillboardClient } from "./components/client";
+import { prismadb } from "@/lib/db";
+import { format } from "date-fns";
+const BillboardPage = async ({ params }: { params: { storeId: string } }) => {
+  const bilboard = await prismadb.billboard.findMany({
+    where: {
+      storeId: params.storeId,
+    },
+    orderBy: {
+      createdAt: "desc",
+    },
+  });
 
-const BillboardPage = () => {
+  const formattedData = bilboard.map((data:any) => {
+    return {
+      id:data.id,
+      label: data.label,
+      imageUrl: data.imageUrl,
+      createdAt: format(data.createdAt,"MMMM do, yyyy"),
+    };
+  });
+
   return (
     <div className="flex-col px-4">
       <div className="flex-1 space-y-4 p-8 pt-6">
-        <BillboardClient />
+        <BillboardClient data={formattedData} />
       </div>
     </div>
   );
