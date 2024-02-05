04982c075b064420c1f3b5444777b6e0d3bf0929
Yanuarprayoga9 <yanuarprayogat@gmail.com>
2024-02-05 19:55:44 +0700

    feat: complete crud bllboard

--- app/(dashboard)/[storeid]/(routes)/billboards/components/cell-action.tsx
+++ app/(dashboard)/[storeid]/(routes)/billboards/components/cell-action.tsx
@@ -1,3 +1,4 @@
+"use client";
 import React, { useState } from "react";
 import { BillboardColumns } from "./columns";
 import {
@@ -14,27 +15,46 @@ import {
 import { Separator } from "@/components/ui/separator";
 import toast from "react-hot-toast";
 import { AlertModal } from "@/components/modals/alert-modal";
+import axios from "axios";
+import { redirect, useParams } from "next/navigation";
 interface CellActionProps {
   data: BillboardColumns;
 }
 const CellAction: React.FC<CellActionProps> = ({ data }) => {
+  const params = useParams();
   const [open, setOpen] = useState<boolean>(false);
   const [loading, setLoading] = useState<boolean>(false);
 
-  const onDelete = () => {
-    console.log("onclick")
-  }
+  const onDelete = async () => {
+    try {
+      setOpen(false);
+      setLoading(true);
+      const res = await axios.delete(
+        `/api/${params.storeId}/billboards/${data.id}`
+      );
+      console.log(res);
+      toast.success("billboard deleted");
+    } catch (error) {
+      toast.error("Something went wrong");
+      console.log(error);
+    } finally {
+      setLoading(false);
+    }
+  };
   const onCopy = (id: any) => {
     navigator.clipboard.writeText(id);
     toast.success("Billboard id copied to the clipboard");
   };
+  const onUpdate =  () => {
+     window.location.assign(`/${params.storeId}/billboards/${data.id}`);
+  };
   return (
     <DropdownMenu>
       <AlertModal
-      isOpen={open}
-      onClose={()=>setOpen(false)}
-      onConfirm={onDelete}
-      loading={loading}
+        isOpen={open}
+        onClose={() => setOpen(false)}
+        onConfirm={onDelete}
+        loading={loading}
       />
       <DropdownMenuTrigger asChild>
         <Button variant="ghost">
@@ -46,19 +66,25 @@ const CellAction: React.FC<CellActionProps> = ({ data }) => {
           Actions
         </DropdownMenuLabel>
         <Separator />
-        <DropdownMenuItem onClick={() => onCopy(data.id)} className="hover:cursor-pointer">
+        <DropdownMenuItem
+          onClick={() => onCopy(data.id)}
+          className="hover:cursor-pointer"
+        >
           <div className="flex items-center ml-2 text-sm mb-2">
             <Copy className="mr-2 h-4 w-4" /> Copy id
           </div>
         </DropdownMenuItem>
         <Separator />
-        <DropdownMenuItem className="hover:cursor-pointer">
+        <DropdownMenuItem onClick={onUpdate} className="hover:cursor-pointer">
           <div className="flex items-center ml-2 text-sm mb-2">
             <Edit className="mr-2 h-4 w-4" /> Update
           </div>
         </DropdownMenuItem>
         <Separator />
-        <DropdownMenuItem onClick={()=>setOpen(true)} className="hover:cursor-pointer">
+        <DropdownMenuItem
+          onClick={() => setOpen(true)}
+          className="hover:cursor-pointer"
+        >
           <div className="flex items-center ml-2 text-sm mb-2">
             <Trash className="mr-2 h-4 w-4" /> Delete
           </div>
