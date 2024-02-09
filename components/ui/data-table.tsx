494da875ab9786e924d3f0bd389560461b304f70
Yanuarprayoga9 <yanuarprayogat@gmail.com>
2024-02-09 21:03:27 +0700

    style: update usable data table

--- components/ui/data-table.tsx
+++ components/ui/data-table.tsx
@@ -25,11 +25,13 @@ import { Input } from "./input";
 interface DataTableProps<TData, TValue> {
   columns: ColumnDef<TData, TValue>[];
   data: TData[];
+  searchKey: string;
 }
 
 export function DataTable<TData, TValue>({
   columns,
   data,
+  searchKey,
 }: DataTableProps<TData, TValue>) {
   const [sorting, setSorting] = React.useState<SortingState>([]);
   const [columnFilters, setColumnFilters] = React.useState<ColumnFiltersState>(
@@ -54,10 +56,10 @@ export function DataTable<TData, TValue>({
     <div>
       <div className="flex items-center py-2">
         <Input
-          placeholder="Filter Label..."
-          value={(table.getColumn("label")?.getFilterValue() as string) ?? ""}
+          placeholder="search"
+          value={(table.getColumn(searchKey)?.getFilterValue() as string) ?? ""}
           onChange={(event) =>
-            table.getColumn("label")?.setFilterValue(event.target.value)
+            table.getColumn(searchKey)?.setFilterValue(event.target.value)
           }
           className="max-w-sm"
         />
