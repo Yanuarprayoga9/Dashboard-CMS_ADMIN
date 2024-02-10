97c274274b08d57862a4397906b1c278a2371820
Yanuarprayoga9 <yanuarprayogat@gmail.com>
2024-02-10 10:48:15 +0700

    feat: working on size form

--- app/(dashboard)/[storeid]/(routes)/sizes/[sizeId]/page.tsx
+++ app/(dashboard)/[storeid]/(routes)/sizes/[sizeId]/page.tsx
@@ -1,6 +1,6 @@
 "use server";
 import React from "react";
-import { BillboardForm } from "./components/billboard-form";
+import { SizeForm } from "./components/size-form";
 import { auth } from "@clerk/nextjs";
 import { redirect } from "next/navigation";
 import { prismadb } from "@/lib/db";
@@ -8,21 +8,20 @@ import { prismadb } from "@/lib/db";
 const BllboardPage = async ({
   params,
 }: {
-  params: { storeId: string; billboardId: string };
+  params: { storeId: string; sizeId: string };
 }) => {
   const { userId } = auth();
   if (!userId) redirect("/sign-in");
-  const billboard = await prismadb.billboard.findFirst({
+  const size = await prismadb.size.findFirst({
     where: {
-      id: params.billboardId,
+      id: params.sizeId,
       storeId: params.storeId,
     },
   });
-  console.log(billboard);
   return (
     <div className="flex flex-col p-4">
       <div className="flex-1 space-y-4 p-8 pt-6">
-        <BillboardForm initialData={billboard} />
+        <SizeForm initialData={size} />
       </div>
     </div>
   );
